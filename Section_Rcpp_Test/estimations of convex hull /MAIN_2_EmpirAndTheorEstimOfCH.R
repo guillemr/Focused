711d88222a4bb6ce2203a4475dee822fb1edf7c9
Liudmila Pishchagina <77454431+lpishchagina@users.noreply.github.com>
2023-09-18 11:06:47 +0200

    update plots

--- Section_Rcpp_Test/estimations of convex hull /MAIN_2_EmpirAndTheorEstimOfCH.R
+++ Section_Rcpp_Test/estimations of convex hull /MAIN_2_EmpirAndTheorEstimOfCH.R
@@ -1,7 +1,7 @@
 ################################################################################
 ##     Figures Number of Facets and Vertices  of Convex Hull                  ##
 ##     Empirical results (using MAIN_1) and theoretical  estimations          ##
-##     by Stirling numbers. Gaussian and Poisson Models (attention : 1h)      ##
+##     by Stirling numbers. Gaussian and Poisson Models                       ##
 ################################################################################
 
 #packages-----------------------------------------------------------------|
@@ -33,6 +33,59 @@ namedim_ <- paste("p =", dim_)
 #number of simulations
 nbSimus_ <- 100
 
+u_n <- c(rep(2 * part_sum_k_n(1,n_), 
+             each = nbSimus_),
+         rep(2 * (part_sum_k_n(1,n_))^2 - 
+               2 * part_sum_k_n(2,n_), 
+             each = nbSimus_),
+         rep(2 * (part_sum_k_n(1,n_))^3 - 
+               6 * part_sum_k_n(1,n_) * part_sum_k_n(2,n_) +
+               4 * part_sum_k_n(3,n_), 
+             each = nbSimus_),
+         rep(2*(part_sum_k_n(1,n_))^4 - 
+               12 * (part_sum_k_n(1,n_))^2 * part_sum_k_n(2,n_) +
+               16 * part_sum_k_n(1,n_) * part_sum_k_n(3,n_) +
+               6 * (part_sum_k_n(2,n_))^2 -
+               12 * part_sum_k_n(4,n_), 
+             each = nbSimus_),
+         rep(2 * (part_sum_k_n(1,n_))^5 -
+               20 * (part_sum_k_n(1,n_))^3 * part_sum_k_n(2,n_) +
+               40 * (part_sum_k_n(1,n_))^2 * part_sum_k_n(3,n_) -
+               60 * part_sum_k_n(1,n_) * part_sum_k_n(4,n_) -
+               40 * part_sum_k_n(2,n_) * part_sum_k_n(3,n_) +
+               48 * part_sum_k_n(5,n_), 
+             each = nbSimus_)
+)
+
+v_n <- c(rep(2 * part_sum_k_n(1,n_), each = nbSimus_),
+         rep( (part_sum_k_n(1,n_))^2 - part_sum_k_n(2,n_) + 2, 
+              each = nbSimus_),
+         rep((1/3) * (part_sum_k_n(1,n_))^3 - part_sum_k_n(1,n_) * part_sum_k_n(2,n_) + 
+               (2/3) * part_sum_k_n(3,n_) + 2 * part_sum_k_n(1,n_),
+             each = nbSimus_),
+         rep((1/12) * (part_sum_k_n(1,n_))^4 -
+               (1/2) * (part_sum_k_n(1,n_))^2 * part_sum_k_n(2,n_) +
+               (2/3) * part_sum_k_n(1,n_) * part_sum_k_n(3,n_) +
+               (1/4) * (part_sum_k_n(2,n_))^2 -
+               (1/2) * part_sum_k_n(4,n_) +
+               (part_sum_k_n(1,n_))^2 -
+               part_sum_k_n(2,n_) + 2, 
+             each = nbSimus_),
+         rep((1/60) * (part_sum_k_n(1,n_))^5 -
+               (1/6) * (part_sum_k_n(1,n_))^3 * part_sum_k_n(2,n_) +
+               (1/3) * (part_sum_k_n(1,n_))^2 * part_sum_k_n(3,n_) +
+               (1/4) * part_sum_k_n(1,n_) * (part_sum_k_n(2,n_))^2 -
+               (1/2) * part_sum_k_n(1,n_) * part_sum_k_n(4,n_) -
+               (1/3) * part_sum_k_n(2,n_) * part_sum_k_n(3,n_) +
+               (2/5) * part_sum_k_n(5,n_) +
+               (1/3) * (part_sum_k_n(1,n_))^3 -
+               part_sum_k_n(1,n_) * part_sum_k_n(2,n_) +
+               (2/3) * part_sum_k_n(3,n_) + 
+               2 * part_sum_k_n(1,n_), 
+             each = nbSimus_)
+)
+
+
 for (j in  1 : length(dim_)) {
   results_fs[[j]] <- read.table(file = paste('Fs_dim',dim_[j],"gauss",nbSimus_,'.txt',sep = '_'), row.names = 1)
   results_vs[[j]] <- read.table(file = paste('Vs_dim',dim_[j],"gauss",nbSimus_,'.txt',sep = '_'), row.names = 1)
@@ -56,26 +109,13 @@ res_FS <- data.frame(n = data_frame_FS$n,
                            data_frame_FS[, 5],
                            data_frame_FS[, 6]),
                      Dimension = rep(colnames(data_frame_FS[,-1]), each = nrow(data_frame_FS)))
+
+
+res_FS$u_n <-u_n
 #plot
-Plot_FS <- ggplot(res_FS, aes(x = n, y = y, color = Dimension)) +
-  geom_point( size = 1, shape = 1) +
-  geom_line(aes(x = n, y = 2 * part_sum_k_n(1,n)) , linetype = "dashed",color = "#A6CEE3") +
-  geom_line(aes(x = n, y = 2 * (part_sum_k_n(1,n))^2 - 
-                  2 * part_sum_k_n(2,n)), linetype = "dashed", color = "#1F78B4") +
-  geom_line(aes(x = n, y = 2 * (part_sum_k_n(1,n))^3 - 
-                  6 * part_sum_k_n(1,n) * part_sum_k_n(2,n) +
-                  4 * part_sum_k_n(3,n)), linetype = "dashed", color = "#B2DF8A") +
-  geom_line(aes(x = n, y = 2*(part_sum_k_n(1,n))^4 - 
-                  12 * (part_sum_k_n(1,n))^2 * part_sum_k_n(2,n) +
-                  16 * part_sum_k_n(1,n) * part_sum_k_n(3,n) +
-                  6 * (part_sum_k_n(2,n))^2 -
-                  12 * part_sum_k_n(4,n)), linetype = "dashed", color = "#33A02C") +
-  geom_line(aes(x = n, y = 2 * (part_sum_k_n(1,n))^5 -
-                  20 * (part_sum_k_n(1,n))^3 * part_sum_k_n(2,n) +
-                  40 * (part_sum_k_n(1,n))^2 * part_sum_k_n(3,n) -
-                  60 * part_sum_k_n(1,n) * part_sum_k_n(4,n) -
-                  40 * part_sum_k_n(2,n) * part_sum_k_n(3,n) +
-                  48 * part_sum_k_n(5,n)), linetype = "dashed", color = "#FB9A99") +
+Plot_FS <- ggplot(res_FS, aes(x = n)) +
+  geom_point(aes(y = y, color = Dimension), size = 1, shape = 1) +
+  geom_line(aes(y = u_n, color = Dimension) , linetype = "dashed")  +
   scale_x_continuous("Number of data points, n-1", breaks=c(2^10,2^11,2^12,2^13, 2^14, 2^15, 2^16, 2^17,2^18,2^19,2^20,2^21,2^22,2^23),  trans = "log10",
                      labels = scales::math_format(2^.x, format = log2))+
   scale_y_continuous("Number of facets, Un", breaks=c(1,10,100,10^3, 10^4, 10^5, 10^6),  trans = "log10",
@@ -94,34 +134,11 @@ res_VS <- data.frame(n = data_frame_VS$n,
                            data_frame_VS[, 5],
                            data_frame_VS[, 6]),
                      Dimension = rep(colnames(data_frame_VS[,-1]), each = nrow(data_frame_VS)))
+res_VS$v_n <-v_n
 
-Plot_VS <- ggplot(res_VS, aes(x = n, y = y, color = Dimension)) +
-  geom_point(size = 1, shape = 1) +
-  geom_line(aes(x = n, y = 2 * part_sum_k_n(1,n)),  linetype = "dashed",color= "#A6CEE3") +
-  geom_line(aes(x = n, y = (part_sum_k_n(1,n))^2 -
-                  part_sum_k_n(2,n) + 2),  linetype = "dashed", color = "#1F78B4") +
-  geom_line(aes(x = n, y = (1/3) * (part_sum_k_n(1,n))^3 -
-                  part_sum_k_n(1,n) * part_sum_k_n(2,n) + 
-                  (2/3) * part_sum_k_n(3,n) +
-                  2 * part_sum_k_n(1,n)), linetype = "dashed", color = "#B2DF8A") +
-  geom_line(aes(x = n, y = (1/12) * (part_sum_k_n(1,n))^4 -
-                  (1/2) * (part_sum_k_n(1,n))^2 * part_sum_k_n(2,n) +
-                  (2/3) * part_sum_k_n(1,n) * part_sum_k_n(3,n) +
-                  (1/4) * (part_sum_k_n(2,n))^2 -
-                  (1/2) * part_sum_k_n(4,n) +
-                  (part_sum_k_n(1,n))^2 -
-                  part_sum_k_n(2,n) + 2), linetype = "dashed", color = "#33A02C") +
-  geom_line(aes(x = n, y = (1/60) * (part_sum_k_n(1,n))^5 -
-                  (1/6) * (part_sum_k_n(1,n))^3 * part_sum_k_n(2,n) +
-                  (1/3) * (part_sum_k_n(1,n))^2 * part_sum_k_n(3,n) +
-                  (1/4) * part_sum_k_n(1,n) * (part_sum_k_n(2,n))^2 -
-                  (1/2) * part_sum_k_n(1,n) * part_sum_k_n(4,n) -
-                  (1/3) * part_sum_k_n(2,n) * part_sum_k_n(3,n) +
-                  (2/5) * part_sum_k_n(5,n) +
-                  (1/3) * (part_sum_k_n(1,n))^3 -
-                  part_sum_k_n(1,n) * part_sum_k_n(2,n) +
-                  (2/3) * part_sum_k_n(3,n) + 
-                  2 * part_sum_k_n(1,n)),  linetype = "dashed", color = "#FB9A99") +
+Plot_VS <- ggplot(res_VS, aes(x = n)) +
+  geom_point(aes(y = y, color = Dimension), size = 1, shape = 1) +
+  geom_line(aes(y = v_n, color = Dimension) , linetype = "dashed") +
   scale_x_continuous("Number of data points, n-1", breaks=c(2^10,2^11,2^12,2^13, 2^14, 2^15, 2^16, 2^17,2^18,2^19,2^20,2^21,2^22,2^23),  trans = "log10",
                      labels = scales::math_format(2^.x, format = log2))+
   scale_y_continuous("Number of vertices, Vn", breaks=c(1,10,100,10^3, 10^4),  trans = "log10",
@@ -169,26 +186,12 @@ res_FS <- data.frame(n = data_frame_FS$n,
                            data_frame_FS[, 5],
                            data_frame_FS[, 6]),
                      Dimension = rep(colnames(data_frame_FS[,-1]), each = nrow(data_frame_FS)))
+res_FS$u_n <-u_n
 #plots
-Plot_FS <- ggplot(res_FS, aes(x = n, y = y, color = Dimension)) +
-  geom_point( size = 1, shape = 1) +
-  geom_line(aes(x = n, y = 2 * part_sum_k_n(1,n)) , linetype = "dashed",color = "#A6CEE3") +
-  geom_line(aes(x = n, y = 2 * (part_sum_k_n(1,n))^2 - 
-                  2 * part_sum_k_n(2,n)), linetype = "dashed", color = "#1F78B4") +
-  geom_line(aes(x = n, y = 2 * (part_sum_k_n(1,n))^3 - 
-                  6 * part_sum_k_n(1,n) * part_sum_k_n(2,n) +
-                  4 * part_sum_k_n(3,n)), linetype = "dashed", color = "#B2DF8A") +
-  geom_line(aes(x = n, y = 2*(part_sum_k_n(1,n))^4 - 
-                  12 * (part_sum_k_n(1,n))^2 * part_sum_k_n(2,n) +
-                  16 * part_sum_k_n(1,n) * part_sum_k_n(3,n) +
-                  6 * (part_sum_k_n(2,n))^2 -
-                  12 * part_sum_k_n(4,n)), linetype = "dashed", color = "#33A02C") +
-  geom_line(aes(x = n, y = 2 * (part_sum_k_n(1,n))^5 -
-                  20 * (part_sum_k_n(1,n))^3 * part_sum_k_n(2,n) +
-                  40 * (part_sum_k_n(1,n))^2 * part_sum_k_n(3,n) -
-                  60 * part_sum_k_n(1,n) * part_sum_k_n(4,n) -
-                  40 * part_sum_k_n(2,n) * part_sum_k_n(3,n) +
-                  48 * part_sum_k_n(5,n)), linetype = "dashed", color = "#FB9A99") +
+#plot
+Plot_FS <- ggplot(res_FS, aes(x = n)) +
+  geom_point(aes(y = y, color = Dimension), size = 1, shape = 1) +
+  geom_line(aes(y = u_n, color = Dimension) , linetype = "dashed")  +
   scale_x_continuous("Number of data points, n-1", breaks=c(2^10,2^11,2^12,2^13, 2^14, 2^15, 2^16, 2^17,2^18,2^19,2^20,2^21,2^22,2^23),  trans = "log10",
                      labels = scales::math_format(2^.x, format = log2))+
   scale_y_continuous("Number of facets, Un", breaks=c(1,10,100,10^3, 10^4, 10^5, 10^6),  trans = "log10",
@@ -208,33 +211,19 @@ res_VS <- data.frame(n = data_frame_VS$n,
                            data_frame_VS[, 6]),
                      Dimension = rep(colnames(data_frame_VS[,-1]), each = nrow(data_frame_VS)))
 
-Plot_VS <- ggplot(res_VS, aes(x = n, y = y, color = Dimension)) +
-  geom_point(size = 1, shape = 1) +
-  geom_line(aes(x = n, y = 2 * part_sum_k_n(1,n)),  linetype = "dashed",color= "#A6CEE3") +
-  geom_line(aes(x = n, y = (part_sum_k_n(1,n))^2 -
-                  part_sum_k_n(2,n) + 2),  linetype = "dashed", color = "#1F78B4") +
-  geom_line(aes(x = n, y = (1/3) * (part_sum_k_n(1,n))^3 -
-                  part_sum_k_n(1,n) * part_sum_k_n(2,n) + 
-                  (2/3) * part_sum_k_n(3,n) +
-                  2 * part_sum_k_n(1,n)), linetype = "dashed", color = "#B2DF8A") +
-  geom_line(aes(x = n, y = (1/12) * (part_sum_k_n(1,n))^4 -
-                  (1/2) * (part_sum_k_n(1,n))^2 * part_sum_k_n(2,n) +
-                  (2/3) * part_sum_k_n(1,n) * part_sum_k_n(3,n) +
-                  (1/4) * (part_sum_k_n(2,n))^2 -
-                  (1/2) * part_sum_k_n(4,n) +
-                  (part_sum_k_n(1,n))^2 -
-                  part_sum_k_n(2,n) + 2), linetype = "dashed", color = "#33A02C") +
-  geom_line(aes(x = n, y = (1/60) * (part_sum_k_n(1,n))^5 -
-                  (1/6) * (part_sum_k_n(1,n))^3 * part_sum_k_n(2,n) +
-                  (1/3) * (part_sum_k_n(1,n))^2 * part_sum_k_n(3,n) +
-                  (1/4) * part_sum_k_n(1,n) * (part_sum_k_n(2,n))^2 -
-                  (1/2) * part_sum_k_n(1,n) * part_sum_k_n(4,n) -
-                  (1/3) * part_sum_k_n(2,n) * part_sum_k_n(3,n) +
-                  (2/5) * part_sum_k_n(5,n) +
-                  (1/3) * (part_sum_k_n(1,n))^3 -
-                  part_sum_k_n(1,n) * part_sum_k_n(2,n) +
-                  (2/3) * part_sum_k_n(3,n) + 
-                  2 * part_sum_k_n(1,n)),  linetype = "dashed", color = "#FB9A99") +
+
+res_VS <- data.frame(n = data_frame_VS$n,
+                     y = c(data_frame_VS[, 2],
+                           data_frame_VS[, 3],
+                           data_frame_VS[, 4],
+                           data_frame_VS[, 5],
+                           data_frame_VS[, 6]),
+                     Dimension = rep(colnames(data_frame_VS[,-1]), each = nrow(data_frame_VS)))
+res_FS$u_n <-v_n
+
+Plot_VS <- ggplot(res_VS, aes(x = n)) +
+  geom_point(aes(y = y, color = Dimension), size = 1, shape = 1) +
+  geom_line(aes(y = v_n, color = Dimension) , linetype = "dashed") +
   scale_x_continuous("Number of data points, n-1", breaks=c(2^10,2^11,2^12,2^13, 2^14, 2^15, 2^16, 2^17,2^18,2^19,2^20,2^21,2^22,2^23),  trans = "log10",
                      labels = scales::math_format(2^.x, format = log2))+
   scale_y_continuous("Number of vertices, Vn", breaks=c(1,10,100,10^3, 10^4),  trans = "log10",
@@ -257,4 +246,3 @@ dev.off()
 ################################################################################
 
 
-   
