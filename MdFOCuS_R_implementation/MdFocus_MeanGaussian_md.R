7841b57b5d7776be27adc519ce30fbc49e63577a
gtromano <g.romano@lancaster.ac.uk>
2024-05-29 14:44:17 +0100

    cleaned code and improved documentation

--- MdFOCuS_R_implementation/MdFocus_MeanGaussian_md.R
+++ MdFOCuS_R_implementation/MdFocus_MeanGaussian_md.R
@@ -13,6 +13,7 @@ library(geometry)
 library(purrr)
 library(Rcpp)
 #functions---------------------------------------------------------------|
+
 #------------------------------------------------------------------------|
 #' @title cost_Focus0
 #'
@@ -77,26 +78,16 @@ lr_Focus <- function(left_cumsum, sum_squares){
   sum(left_cumsum [nrow(left_cumsum), 2:ncol(left_cumsum) , drop=FALSE]^2) / abs(left_cumsum[nrow(left_cumsum), 1])
 }
 
-# -------------------------------------# 
-# get global optimum
-
-get_glo_opt <- function(left_cusum, sum_squares, cost=cost_Focus0) {
-  out_costs <- cost(left_cusum, sum_squares)
- out <- list(
-   opt.change = which.min(out_costs),
-   opt.cost = min(out_costs)
- )
- out
-}
 
 #------------------------------------------------------------------------|
 #' cost_lr_partial
 #'
-#' @description Function for computing the likelihood ratio test for increasing density regimes, e.g. change in 1-d, 2-d, ...,
+#' @description Function for computing the likelihood ratio test in each dimention separately, e.g. 
+#' for a change in dimention 1, dimention 2, up to p
 #' 
-#' @param left_cumsum 
-#' @param sum_squares 
-#' @param null_known 
+#' @param left_cumsum cumsum of data at considering time
+#' @param sum_squares cnst for cost at considering time
+#' @param null_known pre-change parameter known (TRUE), assumed at 0, or not known (FALSE). Defaults to not known, e.g. the GLR test
 #'
 #' @return
 #' @export
@@ -117,14 +108,39 @@ cost_lr_partial <- function(left_cumsum, sum_squares, null_known = FALSE){
     indep_cusum <- right_cost + left_cost + null_cost
   }
   
-  #apply(indep_cusum, 1, sort) |> apply(2, cumsum) |> t()
   indep_cusum
 }
 
+# just a wrapper around the previous function to get the cost_lr_partal for pre-change known and assumed at 0
 cost_lr_partial0 <- \(...) cost_lr_partial(..., null_known = T)
 
 
-## the min and argminum
+# -------------------------------------# 
+# get global optimum
+
+#' This function returns the global optimum of a series of statistics, 
+#'
+#' @param left_cumsum cumsum of data at considering time
+#' @param sum_squares cnst for cost at considering time
+#' @param cost the function to compute the cost, either lr_Focus, lr_Focus0, cost_Focus, cost_Focus0
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+get_glo_opt <- function(left_cusum, sum_squares, cost=lr_Focus) {
+  out_costs <- cost(left_cusum, sum_squares)
+ out <- list(
+   opt.change = which.min(out_costs),
+   opt.cost = min(out_costs)
+ )
+ out
+}
+
+
+# This function is necessary to sort the values of the independent dimentions cusum 
+# statistics, and return the partial sums
 cppFunction('
 List sortCumsumSelectedMinArgmin_(NumericMatrix mat, IntegerVector columns) {
     int nrow = mat.nrow();
@@ -173,9 +189,9 @@ List sortCumsumSelectedMinArgmin_(NumericMatrix mat, IntegerVector columns) {
 #' @description
 #' Get the optimal values across increasing density regimes
 #' 
-#' @param left_cusum 
-#' @param sum_squares 
-#' @param cost A cost function, like cost_lr_partial, should output a matrix of dimensions #T x p, where T is the set of opt changepoints considered
+#' @param left_cumsum cumsum of data at considering time
+#' @param sum_squares cnst for cost at considering time
+#' @param cost A cost function, like cost_lr_partial, should output a matrix of dimensions #T x p, where T is the set of changepoint locations considered
 #'
 #' @return
 #' @export
@@ -186,8 +202,6 @@ get_partial_opt <- function(left_cusum, sum_squares, cost=cost_lr_partial, which
 
   sum_of_the_max <- out_costs |> apply(2, min) |> sum()
 
-  #exact_partials <- apply(out_costs, 1, sort) |> apply(2, cumsum) |> t()
-
   out_cpp <- sortCumsumSelectedMinArgmin_(out_costs, c(1, which_par))
 
   out <- list(
@@ -197,20 +211,21 @@ get_partial_opt <- function(left_cusum, sum_squares, cost=cost_lr_partial, which
                  ex = out_cpp$min_vals[2:length(out_cpp$min_vals)])
   )
 
-  # out <- list(
-  #   opt.change = exact_partials[, which_par, drop=F] %>% apply(2, which.min),
-  #   opt.cost = c(m = min(exact_partials[ , 1]),
-  #                sm = sum_of_the_max,
-  #                ex = exact_partials[, which_par, drop=F] %>% apply(2, min))
-  # )
-
-  # out <- list(
-  #   opt.change = apply(out_costs, 2, which.min),
-  #   opt.cost = apply(out_costs, 2, min)
-  # )
   out
 }
 
+#' get_3_opts_reg
+#' This function returns the cost for the three penalty regimes, useful in low-d.
+#' Equivalent to running get_partial_opt with default parameters, but slightly faster
+#'  
+#' @param left_cumsum cumsum of data at considering time
+#' @param sum_squares cnst for cost at considering time
+#' @param cost A cost function, like cost_lr_partial, should output a matrix of dimensions #T x p, where T is the set of changepoint locations considered
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_3_opts_reg <- function(left_cusum, sum_squares, cost=cost_lr_partial) {
   out_costs <- cost(left_cusum, sum_squares)
 
@@ -232,8 +247,8 @@ get_3_opts_reg <- function(left_cusum, sum_squares, cost=cost_lr_partial) {
 #'
 #' @description  MdFOCuS algorithm for Gaussian model (change in mean)(using the convex hull algorithm of Quickhull)
 #' @param data p-variate time series
-#' @param fun.cost type of functional cost : cost_Focus0 or cost_Focus.
-#' By defult, fun.cost = cost_Focus0 (pre-change mean is known and is equal 0).
+#' @param get_opt_cost type of functional cost : get_glo_opt or get_partial_opt
+#' By defult, get_opt_cost = get_glo_opt (optimum value across all p dimentions with pre-change mean unknown, the GLR test).
 #' @param common_difference_step beta parameter (by default, beta = 1)
 #' @param common_ratio_step alpha parameter (by default, alpha = 2)
 #' @param first_step_qhull the first step for pruning (using convex hull)
@@ -259,6 +274,10 @@ FocusCH <- function(data,
   ##get data parameters
   n <- nrow(data)
   p <- ncol(data)
+
+  if (p > 5) {
+    warning("For sequences of dimention greater than 5, it would be better to use the 2d convex hull approximation. E.g. FocusCH_HighDim(...)")
+  }
   
   ## get cnsts for costs
   data_cumsum_square <- cumsum(rowSums(data^2))
@@ -321,7 +340,28 @@ FocusCH <- function(data,
   return(list_cand)
 }
 
-
+#------------------------------------------------------------------------|
+#' @title FocusCH_HighDim
+#'
+#' @description  High-dimentional MdFOCuS algorithm for Gaussian model (change in mean)(using a projection approximation of the convex hull algorithm, via Quickhull)
+#' @param data p-variate time series
+#' @param get_opt_cost type of functional cost : get_glo_opt or get_partial_opt
+#' By defult, get_opt_cost = get_glo_opt (optimum value across all p dimentions with pre-change mean unknown, the GLR test).
+#' @param common_difference_step beta parameter (by default, beta = 1)
+#' @param common_ratio_step alpha parameter (by default, alpha = 2)
+#' @param first_step_qhull the first step for pruning (using convex hull)
+#' By default, first_step_qhull = p + 5
+#' @param dim_indexes dimentions on which project the hull for the high-dimentional pruning. By default, 2d projections indexes are returned. 
+#' @return a list of  elements  = (index, nb, nb_at_step, opt.cost, opt.change).
+#'
+#' \describe{
+#' \item{\code{index}}{is the set of candidates.}
+#' \item{\code{nb}}{is the number of candidates.}
+#' \item{\code{nb_at_step}}{is a vector of the number of candidates at each iteration.}
+#' \item{\code{opt.cost}}{is a vector of the functional cost at each iteration.}
+#' \item{\code{opt.change}}{is a number of candidates at each iteration (vector).}
+#' \item{\code{opt.cost}}{is a cost or -likelihood-ratio vector of the optimal changepoint candidate at each iteration.}
+#' }
 FocusCH_HighDim <- function(data,
                     get_opt_cost = get_glo_opt,
                     common_difference_step = 1,
@@ -381,9 +421,6 @@ FocusCH_HighDim <- function(data,
         unique() |>
         sort()
       
-      
-      #hull <- convhulln(left_mean) # get convex hull of {P(i)} i in set of candidates
-      #on_the_hull <- sort(unique(as.vector(hull))) # get vertices
       list_cand$nb <- length(on_the_hull) #get number of vertices
       list_cand$index[1:list_cand$nb] <- index_cand[on_the_hull]; #update the set of candidates
       ## update next_step_qhull
@@ -403,226 +440,7 @@ FocusCH_HighDim <- function(data,
   return(list_cand)
 }
 
-FocusCH_HighDim_OPT <- function(data,
-                    get_opt_cost = get_glo_opt,
-                    common_difference_step = 1,
-                    common_ratio_step = 2,
-                    first_step_qhull = ncol(data) + 5,
-                    threshold = Inf,
-                    dim_indexes = map2(seq(0, ncol(data)-1, by=2), seq(1, ncol(data), by=2), \(f, s) c(f, s %% ncol(data)) + 1)) {
-  
-  update_hull <- function(l_c) {
-    if (l_c$nb >= l_c$next_step_qhull | i == n) {
-      index_cand <- l_c$index[1:l_c$nb]
-      local_left_mean <- data_left_cumsum[index_cand, c(1, l_c$i_d)]
-      hull <- convhulln(local_left_mean) # get convex hull of {P(i)} i in set of candidates
-      on_the_hull <- sort(unique(as.vector(hull))) # get vertices
-      l_c$nb <- length(on_the_hull) # get number of vertices
-      l_c$index[1:l_c$nb] <- index_cand[on_the_hull] # update the set of candidates
-      ## update next_step_qhull
-      l_c$next_step_qhull <- common_ratio_step * l_c$nb + common_difference_step
-    }
-    return(l_c)
-  }
-
-  # Initialization-------------
-  ##get data parameters
-  n <- nrow(data)
-  p <- ncol(data)
-  
-  ## get cnsts for costs
-  data_cumsum_square <- cumsum(rowSums(data^2))
-  
-  ##get points P(i), i in {1,n}
-  data_left_cumsum  <- apply(cbind (1, data), 2, cumsum)
-  
-
-  list_cand <- map(dim_indexes, \(i_d) list(index = integer(n), # set of candidates
-                                            nb = 0, #number of candidates
-                                            nb_at_step = integer(n), #vector of the number of candidates at each iteration
-                                            i_d = i_d + 1,
-                                            next_step_qhull = first_step_qhull)
-  )
-  
-  opt_list <- list(
-    opt.cost = vector(mode = "list", length = n),   #vector of the functional cost at each iteration
-    opt.change = vector(mode = "list", length = n)  #vector of the optimal changepoint candidate at each iteration
-    )
-
-
-  ##First iteration
-  list_cand <- map(list_cand, \(l_c) {
-    l_c$nb                  <- l_c$nb + 1
-    l_c$index[l_c$nb] <- 1
-    l_c$nb_at_step[1]       <- 1
-    l_c
-  })
-
-  
-  #------------------------------------
-  #For loop----------------------------
-  for (i in 2:n) {  # start at 2
-    ## add a candidate
-    list_cand <- map(list_cand, \(l_c) {
-      l_c$nb                  <- l_c$nb + 1
-      l_c$index[l_c$nb]       <- i
-      l_c$nb_at_step[i]       <- l_c$nb
-      l_c
-    })
-    
-    ##First step: search of optimal changepoint candidate (index and maximization)
-
-    glo_index_cand <- map(list_cand, \(l_c) l_c$index[1:l_c$nb]) |> 
-                        unlist() |>
-                        unique()
- 
-    glo_left_mean  <- data_left_cumsum[glo_index_cand, ] #get cumsum for cost at time i
-    out <- get_opt_cost(glo_left_mean, data_cumsum_square[i]) # get optimal cost
-    opt_list$opt.change[[i]] <- out$opt.change
-    opt_list$opt.cost[[i]] <- out$opt.cost
-
-    # for debug reasons only
-    # opt_list$opt.change[[i]] <- rep(0, length(threshold))
-    # opt_list$opt.cost[[i]] <- rep(0, length(threshold))
-
-    ## Second step: Pruning by the Quickhull algorithm from the package "geometry"
-    list_cand <- map(list_cand, update_hull)
-
-    
-    ## if the (minus) optimal cost is over the threshold end the algorithm
-    if(length(opt_list$opt.cost[[i]]) == length(threshold)) {
-      change_detected <- sum(- opt_list$opt.cost[[i]] >= threshold)
-    } else {
-      warning("The number of statistics to test does not match the length of the threshold")
-    }
-
-    if (change_detected) {
-      break
-    }
-    
-  }
-  return(opt_list)
-}
 
 ################################################################################
 ########################### END ################################################
 ################################################################################
-
-if(F) {      
-  source("Section_4_3_2_OCDlike_Simulation/helper_functions.R")
-    
-  y_nc <- generate_sequence(n = 1000, p = 3, cp = 999, magnitude = 1, dens = 1, seed = 123) |> t()
-
-  # with the unkown loss 
-  system.time(out <- FocusCH(y_nc, get_opt_cost = \(...) get_glo_opt(..., cost=lr_Focus0), threshold = Inf))
-  - head(out$opt.cost |> unlist())
-}
-
-# testing stuff
-if (F) {
-  library(tidyverse)
-  library(future)
-  library(furrr)
-  plan(multisession, workers=18)
-  
-  source("Section_4_3_2_OCDlike_Simulation/helper_functions.R")
-  
-  y_nc <- generate_sequence(n = 5000, p = 3, cp = 4999, magnitude = 0, dens = 0, seed = 42) |> t()
-
-  # with the known loss  
-  cost_lr_partial0 <- \(...) cost_lr_partial(..., null_known = T)
-  
-  out <- FocusCH(y_nc, get_opt_cost = \(...) get_partial_opt(..., cost = cost_lr_partial0), threshold = rep(Inf, 3))
-  out$opt.cost
-
-  # with the unkown loss 
-  out <- FocusCH(y_nc, get_opt_cost = get_partial_opt, threshold = rep(Inf, 3))
-  out$opt.cost
-  
-  # tuning the thresholds
-  get_one_run <- function(seed, n = 100, p = 3, v_density = 1:p) {
-    y_nc <- generate_sequence(n = n, p = p, cp = n-1, magnitude = 0, dens = 0, seed = seed) |> t()
-    out <- FocusCH(y_nc, get_opt_cost = get_partial_opt, threshold = rep(Inf, p))
-    -reduce(out$opt.cost, rbind)[, v_density] |> apply(2, max)
-  }
-  
-  out1 <- future_map(1:100, get_one_run, p = 3) |> reduce(rbind)
-  apply(out1, 2, quantile, prob=exp(-1))
-  
-  
-  ## unknown
-  get_one_run <- function(seed, n = 100, p = 3, v_density = 1:p) {
-    y_nc <- generate_sequence(n = n, p = p, cp = n-1, magnitude = 0, dens = 0, seed = seed) |> t()
-    out <- FocusCH(y_nc, get_opt_cost = \(...) get_partial_opt(..., cost = cost_lr_partial0), threshold = rep(Inf, p))
-    -reduce(out$opt.cost, rbind)[, v_density] |> apply(2, max)
-  }
-  
-  out2 <- future_map(1:100, get_one_run, p = 3) |> reduce(rbind)
-  apply(out2, 2, quantile, prob=exp(-1))
-}
-
-
-# testing high dimentional
-
-if (F) {
-  library(tidyverse)
-  library(future)
-  library(furrr)
-  plan(multisession, workers=18)
-  
-  source("Section_4_3_2_OCDlike_Simulation/helper_functions.R")
-  
-  p <- 100
-  N <- 2000
-  y = generate_sequence(n = N, p = p, cp = 500, magnitude = 0, dens = 0, seed = 42)  
-
-  # focus
-  res <- FOCuS_multi_JMLR(y, c(Inf, Inf), mu0 = rep(0, p))
-  data.frame(max = max(res$maxs), sum = max(res$sums))
-
-  data <- t(y) # trasposing as the current prototype reads nxp (rather then pxn)
-  system.time(res <- FocusCH_HighDim(data, get_opt_cost = \(...) get_partial_opt(..., cost=cost_lr_partial0, which_par = c(5, 25, 100)), common_ratio_step = 2, threshold = rep(Inf, 5)))
-  - (res$opt.cost |> reduce(rbind)) |> apply(2, max)
-
-  ocd_det <- ocd_known(c(Inf, Inf, Inf), rep(0, p), rep(1, p))
-  system.time(r <- ocd_detecting(y, ocd_det))
-}
-
-# testing fast high dimentional
-
-if (F) {
-  library(tidyverse)
-  library(future)
-  library(furrr)
-  library(Rcpp)
-  plan(multisession, workers=18)
-  
-  source("exemple_Rcpp.R")
-
-  source("Section_4_3_2_OCDlike_Simulation/helper_functions.R")
-  
-  p <- 50
-  N <- 5000
-  y = generate_sequence(n = N, p = p, cp = N-1, magnitude = 0, dens = 0, seed = 123)
-
-
-
-  data <- t(y) # trasposing as the current prototype reads nxp (rather then pxn)
-  #res <- FocusCH_HighDim_OPT(data, get_opt_cost = \(...) get_partial_opt(..., cost=cost_lr_partial0, which_par = c(5, 25, 100)), common_ratio_step = 2, threshold = rep(Inf, 5))
-  system.time(res1 <- FocusCH_HighDim_OPT(data, get_opt_cost = \(...) get_partial_opt(..., cost=cost_lr_partial0, which_par = c(5, 25, 100)), common_ratio_step = 2, threshold = rep(Inf, 5)))
-  - (res1$opt.cost |> reduce(rbind)) |> apply(2, max)
-  system.time(res <- FocusCH_HighDim(data, get_opt_cost = \(...) get_partial_opt(..., cost=cost_lr_partial0, which_par = c(5, 25, 100)), common_ratio_step = 1.5, threshold = rep(Inf, 5)))
-  - (res$opt.cost |> reduce(rbind)) |> apply(2, max)
-
-  ocd_det <- ocd_known(c(Inf, Inf, Inf), rep(0, p), rep(1, p))
-  system.time(r <- ocd_detecting(y, ocd_det))
-
-
-  # res3 <- FocusCH_HighDim(data, get_opt_cost = \(...) get_partial_opt(..., cost=cost_lr_partial0, which_par = c(5, 25, 100)),
-  #                                         common_ratio_step = 1.2,
-  #                                         dim_indexes = as.list(1:ncol(data)),
-  #                                         threshold = rep(Inf, 5))
-  # - (res3$opt.cost |> reduce(rbind)) |> apply(2, max)
-
-
-}
